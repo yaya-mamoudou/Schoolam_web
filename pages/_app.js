c8ebb02922f443534d276e268321d8f58aacf0c6
yaya-mamoudou <yayamamoudou0@gmail.com>
2022-03-13 08:31:53 +0100

    Added fonts(OpenSans and Yantramanav

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Provider } from 'react-redux';
 import { useStore } from '../redux/store';
 import '../public/i18n/i18n';
+import Head from 'next/head';
 
 export async function getStaticProps({ locale }) {
   return {
@@ -19,6 +20,34 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Provider store={store}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          crossorigin="anonymous"
+        ></script>
+
+        {/* Karma fonts  */}
+        {/* USE IN CSS FILE */}
+        {/* 
+          font-family: 'Karma', serif;
+          font-family: 'Yantramanav', sans-serif;
+         */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Karma:wght@300&family=Yantramanav:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
