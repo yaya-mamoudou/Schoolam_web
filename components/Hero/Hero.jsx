48ffad2717c68ce97f3fb5aeba9d5934a388bf2c
yaya-mamoudou <yayamamoudou0@gmail.com>
2022-03-29 08:10:43 +0100

    Made hero responsive

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -6,18 +6,22 @@ import Image from 'next/image'
 export default function Hero() {
   return (
     <div className={`${styles.container}`}>
-      <div className="container h-100 d-flex position-relative">
-        <div className="align-self-center">
+      <div className="container h-100 d-flex flex-column flex-lg-row position-relative">
+        <div style={{zIndex:2}} className="align-self-center ">
           <h1 className={`${styles.header}`}>Search your <span style={{color:'var(--purple)'}}>dream University</span></h1>
           <div className={`${styles.text} mt-2`}>Discover universities that you never knew about.</div>
           <div className={`${styles.text} mb-5`}>Search schools like never before.</div>
-          <div className="d-flex">
+          <div className={`d-flex ${styles.btn_section}`}>
             <Button className={`px-5 py-2 ${styles.btn} me-5`} text={'Get started'} />
             <Image height={70} width={89} src={'/images/Arrow.png'}/>
           </div>
         </div>
-        <div style={{ bottom: 0, right: 0,position:'absolute',height:350,width:500 }}>
-          <Image layout="fill" className={`${styles.img}`} height={500}  width={400} src={'/images/boy_with_pencil.png'}/>
+        <div style={{ bottom: 0, right: 0, height: 300, width: 400 }} className={`${styles.img_box}`}>
+          <Image layout="fill" className={`${styles.img}`} height={400} width={400} src={'/images/boy_with_pencil.png'} />
+          <div className={`${styles.img_box2}`}>
+          <Image objectFit="contain" className={`${styles.img2}`} height={80} width={70} src={'/images/purple_circles.png'} />
+
+          </div>
         </div>
       </div>
     </div>
